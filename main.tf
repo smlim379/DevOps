0b492f1bb0e095ec1cf4030106955f6f73d3a422
smlim379 <103095321+smlim379@users.noreply.github.com>
2022-04-21 12:50:54 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,6 @@ terraform {
 }
 
 provider "aws" {
-  access_key = ["${ var.AWS_ACCESS_KEY }"]
-  secret_key = ["${ var.AWS_SECRET_ACCESS_KEY }"]
   profile    = "default"
   region     = "ap-northeast-1"
 }
